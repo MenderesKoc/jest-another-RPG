110b53249535d25acd72ae40a9d6fa235bbcbf4b
mndrs <mndrs.kc@gmail.com>
2022-02-21 21:54:20 -0500

    player constructor finish

--- __tests__/Player.test.js
+++ __tests__/Player.test.js
@@ -2,7 +2,6 @@ const Player = require('../lib/Player.js');
 const Potion = require('../lib/Potion');
 
 jest.mock('../lib/Potion');
-
 console.log(new Potion());
 
 test('creates a player object', () => {
